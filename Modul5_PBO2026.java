1b7c1e9d652ffc006dcd989b981596c0ead40da0
kairanisaajmalia-png <kairanisaajmalia@gmail.com>
2026-03-12 10:40:46 +0700

    Add files via upload

---
+++ Modul5_PBO2026.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul5_pbo2026;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class Modul5_PBO2026 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
