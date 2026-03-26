25c2f70aa3a3a87d887e678207921507018af238
kairanisaajmalia-png <kairanisaajmalia@gmail.com>
2026-03-26 10:30:06 +0700

    Refactor input method to use BufferedReader

--- Nomor_2.java
+++ Nomor_2.java
@@ -4,27 +4,48 @@
  */
 package latihan_260326;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Nomor_2 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print("Masukkan angka (1-10): ");
-        int angka = input.nextInt();
+        System.out.print("Masukkan angka 1-10: ");
+        int angka = Integer.parseInt(br.readLine());
 
         switch (angka) {
-            case 1: System.out.println("Satu"); break;
-            case 2: System.out.println("Dua"); break;
-            case 3: System.out.println("Tiga"); break;
-            case 4: System.out.println("Empat"); break;
-            case 5: System.out.println("Lima"); break;
-            case 6: System.out.println("Enam"); break;
-            case 7: System.out.println("Tujuh"); break;
-            case 8: System.out.println("Delapan"); break;
-            case 9: System.out.println("Sembilan"); break;
-            case 10: System.out.println("Sepuluh"); break;
-            default: System.out.println("Invalid number");
+            case 1:
+                System.out.println("Satu");
+                break;
+            case 2:
+                System.out.println("Dua");
+                break;
+            case 3:
+                System.out.println("Tiga");
+                break;
+            case 4:
+                System.out.println("Empat");
+                break;
+            case 5:
+                System.out.println("Lima");
+                break;
+            case 6:
+                System.out.println("Enam");
+                break;
+            case 7:
+                System.out.println("Tujuh");
+                break;
+            case 8:
+                System.out.println("Delapan");
+                break;
+            case 9:
+                System.out.println("Sembilan");
+                break;
+            case 10:
+                System.out.println("Sepuluh");
+                break;
+            default:
+                System.out.println("Invalid number");
         }
     }
 }
