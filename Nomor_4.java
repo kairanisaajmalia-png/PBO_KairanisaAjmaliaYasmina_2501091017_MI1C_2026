4649d2274f2ca3c3e5a2a9be98464221915fe56f
kairanisaajmalia-png <kairanisaajmalia@gmail.com>
2026-03-26 10:55:48 +0700

    Add files via upload

---
+++ Nomor_4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan_260326;
+
+public class Nomor_4 {
+    public static void main(String[] args) {
+
+        String nama = "Kairanisa Ajmalia Yasmina";
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(nama);
+        }
+    }
+}
