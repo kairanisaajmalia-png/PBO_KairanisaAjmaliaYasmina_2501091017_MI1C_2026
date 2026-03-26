e905ea1ad8c1f6ee8ca5bb99c1216868d7887d38
kairanisaajmalia-png <kairanisaajmalia@gmail.com>
2026-03-26 10:28:44 +0700

    Refactor input handling to use BufferedReader

--- Nomor_1.java
+++ Nomor_1.java
@@ -4,25 +4,38 @@
  */
 package latihan_260326;
 
-    import java.util.Scanner;
+import java.io.*;
 
 public class Nomor_1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
 
-        System.out.print("Masukkan angka (1-10): ");
-        int angka = input.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        if (angka == 1) System.out.println("Satu");
-        else if (angka == 2) System.out.println("Dua");
-        else if (angka == 3) System.out.println("Tiga");
-        else if (angka == 4) System.out.println("Empat");
-        else if (angka == 5) System.out.println("Lima");
-        else if (angka == 6) System.out.println("Enam");
-        else if (angka == 7) System.out.println("Tujuh");
-        else if (angka == 8) System.out.println("Delapan");
-        else if (angka == 9) System.out.println("Sembilan");
-        else if (angka == 10) System.out.println("Sepuluh");
-        else System.out.println("Invalid number");
+        System.out.print("Masukkan angka 1-10: ");
+        int angka = Integer.parseInt(br.readLine());
+
+        if (angka == 1) {
+            System.out.println("Satu");
+        } else if (angka == 2) {
+            System.out.println("Dua");
+        } else if (angka == 3) {
+            System.out.println("Tiga");
+        } else if (angka == 4) {
+            System.out.println("Empat");
+        } else if (angka == 5) {
+            System.out.println("Lima");
+        } else if (angka == 6) {
+            System.out.println("Enam");
+        } else if (angka == 7) {
+            System.out.println("Tujuh");
+        } else if (angka == 8) {
+            System.out.println("Delapan");
+        } else if (angka == 9) {
+            System.out.println("Sembilan");
+        } else if (angka == 10) {
+            System.out.println("Sepuluh");
+        } else {
+            System.out.println("Invalid number");
+        }
     }
 }
