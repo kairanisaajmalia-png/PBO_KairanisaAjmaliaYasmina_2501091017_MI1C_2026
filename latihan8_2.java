3e8cc7f6dfbb2a977daea3c536c2e5b4f58758ae
kairanisaajmalia-png <kairanisaajmalia@gmail.com>
2026-04-07 15:03:11 +0700

    Add files via upload

---
+++ latihan8_2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class latihan8_2 {
+    public static void main(String[] args) {
+        int i = Integer.parseInt(args[0]);
+        int j = Integer.parseInt(args[1]);
+
+        int a = i + j;
+        int b = i - j;
+        int c = i * j;
+        int d = i / j;
+
+        System.out.println("sum         = " + a);
+        System.out.println("difference  = " + b);
+        System.out.println("product     = " + c);
+        System.out.println("quotient    = " + d);
+    }
+}
