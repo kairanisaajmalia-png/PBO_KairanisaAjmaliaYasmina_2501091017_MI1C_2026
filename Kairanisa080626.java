e37656ed7bc3b9674cbf1ce0c8646123eeecd498
kairanisaajmalia-png <kairanisaajmalia@gmail.com>
2026-04-14 14:41:42 +0700

    Add files via upload

---
+++ Kairanisa080626.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kairanisa080626;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class Kairanisa080626 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i = 10;
+       System.out.println(i);
+       
+       test(i);
+       System.out.println(i);
+    }
+    
+    public static void test (int j){
+        j = 33;
+        
+    }
+    
+}
